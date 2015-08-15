1bc1effe0c16a9459d479117cba1daeaf6dd3785
liweiz <matt.z.lw@gmail.com>
2015-08-14 21:28:02 -0400

    Completed sync function

--- wordsToCtxTransitionAnimation/wordsToCtxTransitionAnimation/WordsToCtxTransitionCtl.swift
+++ wordsToCtxTransitionAnimation/wordsToCtxTransitionAnimation/WordsToCtxTransitionCtl.swift
@@ -26,34 +26,10 @@ func getMovableOneTextLineView(origin: CGPoint, visiableGlyphRange: NSRange, ctx
     
 }
 
-class MovableOneTextLineView: UIScrollView {
-    let textView: UITextView
-    // Initial contentOffset on x-axis.
-    let baseContentOffsetX: CGFloat
-    // Store distance moved triggered by this on x-axis.
-    var extraXTiggered = CGFloat(0)
-    var isTrigger = false
-    // Used by lineExtraView to hide the visiable part.
-    var visiableCharacterRange: NSRange!
-    var visiableGlyphRange: NSRange {
-        return textView.layoutManager.glyphRangeForCharacterRange(visiableCharacterRange, actualCharacterRange: nil)
-    }
-    var visiableGlyphsRectX: CGFloat {
-        return textView.frame.origin.x + textView.textContainer.lineFragmentPadding + textView.textContainerInset.left + textView.layoutManager.boundingRectForGlyphRange(visiableGlyphRange, inTextContainer: textView.textContainer).origin.x
-    }
-    init(textViewToInsert: UITextView, rect: CGRect) {
-        super.init(frame: rect)
-        decelerationRate = UIScrollViewDecelerationRateFast
-        contentSize = CGSizeMake(textViewToInsert.frame.width * 3, textViewToInsert.frame.height)
-        textView = textViewToInsert
-        addSubview(textView)
-    }
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-}
+
 
 // CtxLinesCtl(a set of functions) to have the lastest info about MovableOneTextLineViews for a certain context and refresh them
+
 func getLinesInfoInWordsView(wordsView: UITextView) -> (glyphRanges: [NSRange], lineRects: [CGRect]) {
     
 }
@@ -63,7 +39,56 @@ func getLines(glyphRanges: [NSRange], lineRects: [CGRect]) -> [MovableOneTextLin
 }
 
 // SyncCtl(a set of functions) to control the movement of MovableOneTextLineViews
-func refreshBaseContentOffsetXs()
+struct Lines {
+    var main: [MovableOneTextLineView]
+    var extra: [MovableOneTextLineView]
+    
+    
+    
+    func sync(line: MovableOneTextLineView, deltaX: CGFloat) {
+        let linesToSync = findLinesToSync(line)
+        if linesToSync.count > 0 {
+            let baseXs = getBaseXs(linesToSync)
+            updateContentOffset(linesToSync, newXs: baseXs.map { $0 + deltaX })
+        }
+    }
+    
+    func getBaseXs(lines: [MovableOneTextLineView]) -> [CGFloat] {
+        return lines.map { return $0.contentOffset.x }
+    }
+    
+    func findLinesToSync(afterLine: MovableOneTextLineView) -> [MovableOneTextLineView] {
+        if let r = findElements(afterLine, inArray: main) {
+            return r + findElements(main.indexOf(afterLine)!, inArray: extra)!
+        } else {
+            return findElements(afterLine, inArray: extra)! + findElements(extra.indexOf(afterLine)!, inArray: main)!
+        }
+        return [MovableOneTextLineView]()
+    }
+}
+
+func findElements<T: Equatable>(beyondElement: T, inArray: [T]) -> [T]? {
+    if let i = inArray.indexOf(beyondElement) {
+        return inArray.filter { inArray.indexOf($0) > i }
+    }
+    return nil
+}
+
+func findElements<T: Equatable>(beyondIndex: Int, inArray: [T]) -> [T]? {
+    return inArray.count > beyondIndex ? inArray.filter { inArray.indexOf($0) > beyondIndex } : nil
+}
+
+func findTextLines(forBaseContentOffsetXs: [CGFloat], inCtxLines: [MovableOneTextLineView]) -> [MovableOneTextLineView] {
+    return inCtxLines.filter { inCtxLines.indexOf($0) > inCtxLines.count - forBaseContentOffsetXs.count - 1 }
+}
+
+func updateContentOffset(lines: [MovableOneTextLineView], newXs: [CGFloat]) {
+    lines.forEach { $0.contentOffset = CGPointMake(newXs[lines.indexOf($0)!], $0.contentOffset.y) }
+}
+
+func refreshMovableOneTextLineView(baseOffsetXs: [CGFloat], deltaOffsetX: CGFloat, lineViews: [MovableOneTextLineView]) {
+    lineViews.forEach { $0.contentOffset = CGPointMake(baseOffsetXs[lineViews.indexOf($0)!] + deltaOffsetX, $0.contentOffset.y) }
+}
 
 func syncMoves(mainViews: [MovableOneTextLineView], extraViews: [MovableOneTextLineView], animated: Bool = true) {
     for m in mainViews {
@@ -82,5 +107,27 @@ func syncMoves(mainViews: [MovableOneTextLineView], extraViews: [MovableOneTextL
                 }
             }
         }
+    }
+}
+
+class MovableOneTextLineView: UIScrollView {
+    let textView: UITextView
+    // Used by lineExtraView to hide the visiable part.
+    var visiableCharacterRange: NSRange!
+    var visiableGlyphRange: NSRange {
+        return textView.layoutManager.glyphRangeForCharacterRange(visiableCharacterRange, actualCharacterRange: nil)
+    }
+    var visiableGlyphsRectX: CGFloat {
+        return textView.frame.origin.x + textView.textContainer.lineFragmentPadding + textView.textContainerInset.left + textView.layoutManager.boundingRectForGlyphRange(visiableGlyphRange, inTextContainer: textView.textContainer).origin.x
+    }
+    init(textViewToInsert: UITextView, rect: CGRect) {
+        super.init(frame: rect)
+        decelerationRate = UIScrollViewDecelerationRateFast
+        contentSize = CGSizeMake(textViewToInsert.frame.width * 3, textViewToInsert.frame.height)
+        textView = textViewToInsert
+        addSubview(textView)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
 }
