1835616e89e6604ca6390e6d440c42b021af8d7d
liweiz <matt.z.lw@gmail.com>
2016-03-16 01:19:32 -0400

    Rewrote TimeWorker with unit tests

--- TimeWorker/TimeWorker.swift
+++ TimeWorker/TimeWorker.swift
@@ -13,7 +13,7 @@ enum TimeError: ErrorType {
 }
 
 // whenIsNextTime looks for the next time among times explicitly set(e.g., now it's 07, looking for next time firing some task every 15 mins from 00/15/30/45. delayedBy is to delay the task for N seconds.
-func whenIsNextTime(amongMinutePresets: [String], delayedBy: Double = 0) throws -> NSDate {
+func whenIsNextTime(fromTime: NSDate, amongMinutePresets: [String], delayedBy: Double = 0) throws -> NSDate {
     guard amongMinutePresets.count > 0 else {
         throw TimeError.InvalidInput("Invalid input: preset minute digit array should not be empty")
     }
@@ -25,24 +25,23 @@ func whenIsNextTime(amongMinutePresets: [String], delayedBy: Double = 0) throws 
         minsInInt.append(y)
     }
     for x in minsInInt {
-        guard (x >= 0 || x < 60) else { throw TimeError.InvalidInput("Invalid input for minute digit: \(x)") }
+        guard (x >= 0 && x < 60) else { throw TimeError.InvalidInput("Invalid input: minute digit: \(x)") }
     }
     let gregorian = NSCalendar(identifier: NSCalendarIdentifierGregorian)!
-    let now = NSDate()
-    let nowComponents = gregorian.components([NSCalendarUnit.Day, NSCalendarUnit.Hour, NSCalendarUnit.Minute], fromDate: now)
-    let nextMinDigit = try nextMinuteDigit(nowComponents.minute, minPresets: minsInInt)
-    let delta = nextMinDigit - nowComponents.minute
-    let intervalInMinute = Double(delta >= 0 ? delta : 60 - nowComponents.minute + delta)
-    return now.dateByAddingTimeInterval(intervalInMinute * 60)
+    let fromTimeComponents = gregorian.components([NSCalendarUnit.Day, NSCalendarUnit.Hour, NSCalendarUnit.Minute], fromDate: fromTime)
+    let nextMinDigit = try nextMinuteDigit(fromTimeComponents.minute, minPresets: minsInInt)
+    let delta = nextMinDigit - fromTimeComponents.minute
+    let intervalInMinute = Double(delta >= 0 ? delta : 60 - fromTimeComponents.minute + nextMinDigit)
+    return fromTime.dateByAddingTimeInterval(intervalInMinute * 60 + delayedBy)
 }
 
 // nextMinuteDigit returns the next minute digit among the preset minute digits
 func nextMinuteDigit(currentMin: Int, minPresets: [Int]) throws -> Int {
     guard minPresets.count > 0 else {
-        throw TimeError.InvalidInput("Invalid input: empty preset minute digit array")
+        throw TimeError.InvalidInput("Invalid input: preset minute digit array should not be empty")
     }
     for x in minPresets + [currentMin] {
-        guard x >= 0 || x < 60 else {
+        guard x >= 0 && x < 60 else {
             throw TimeError.InvalidInput("Invalid input: minute digit: \(x)")
         }
     }
--- TimeWorker/TimeWorkerTests/TimeWorkerTests.swift
+++ TimeWorker/TimeWorkerTests/TimeWorkerTests.swift
@@ -26,25 +26,102 @@ class TimeWorkerTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    func testWhenIsNextTime() {
+        let fromTimeComponent = NSDateComponents()
+        fromTimeComponent.year = 2016
+        fromTimeComponent.month = 1
+        fromTimeComponent.day = 21
+        let hour = 18
+        fromTimeComponent.hour = hour
+        fromTimeComponent.minute = 14
+        fromTimeComponent.timeZone = NSTimeZone(abbreviation: "UTC")
+        fromTimeComponent.calendar = NSCalendar(identifier: NSCalendarIdentifierGregorian)!
+        let minutes = [0, 14, 21, 57]
+        let fromTimes = minutes.map {
+            (minute: Int) -> NSDate in
+            fromTimeComponent.minute = minute
+            return fromTimeComponent.date!
+        }
+        struct Test {
+            let testName: String
+            let fromTime: NSDate
+            let amongMinutePresets: [String]
+            let delayedBy: Double
+            let expectedOutput: NSDate?
+            let expectedError: ErrorType?
+        }
+        let amongMinutePresetsString = ["0", "20", "40", "45", "55"]
+        let amongMinutePresets = [0, 20, 40, 45, 55]
+        let nextTimes = amongMinutePresets.map {
+            (minute: Int) -> NSDate in
+            fromTimeComponent.minute = minute
+            if fromTimeComponent.minute == 0 {
+                fromTimeComponent.hour += 1
+            } else {
+                fromTimeComponent.hour = hour
+            }
+            return fromTimeComponent.date!
+        }
+        let toTestExpectingNoError = [
+            Test(testName: "NoError, greater minute digit found no delay", fromTime: fromTimes[1], amongMinutePresets: amongMinutePresetsString, delayedBy: 0, expectedOutput: nextTimes[1], expectedError: nil),
+            Test(testName: "NoError, less minute digit found no delay", fromTime: fromTimes[3], amongMinutePresets: amongMinutePresetsString, delayedBy: 0, expectedOutput: nextTimes[0], expectedError: nil),
+            Test(testName: "NoError, digit from preset no delay", fromTime: fromTimes[0], amongMinutePresets: amongMinutePresetsString, delayedBy: 0, expectedOutput: nextTimes[1], expectedError: nil)
+        ]
+        var i = 0
+        let testTargetName = "whenIsNextTime *** "
+        for t in toTestExpectingNoError {
+            print("TEST_NAME [\(i)]: " + testTargetName + t.testName + " *** START")
+            XCTAssertEqual(try! whenIsNextTime(t.fromTime, amongMinutePresets: t.amongMinutePresets, delayedBy: 0), t.expectedOutput)
+            print("TEST_NAME [\(i)]: " + testTargetName + t.testName + " *** END")
+            i += 1
+        }
+        let toTestExpectingError = [
+            Test(testName: "Error, empty Presets input", fromTime: NSDate(), amongMinutePresets: [], delayedBy: 0, expectedOutput: nil, expectedError: TimeError.InvalidInput("Invalid input: preset minute digit array should not be empty")),
+            Test(testName: "Error, out of range input", fromTime: NSDate(), amongMinutePresets: ["60"], delayedBy: 0, expectedOutput: nil, expectedError: TimeError.InvalidInput("Invalid input: minute digit: 60")),
+            Test(testName: "Error, amongMinutePresets not convertible to [Int] from [String]", fromTime: NSDate(), amongMinutePresets: ["60"], delayedBy: 0, expectedOutput: nil, expectedError: TimeError.InvalidInput("Invalid input: preset minute digit array failed to be converted to [Int]"))
+        ]
+        var j = 0
+        for t in toTestExpectingError {
+            print("TEST_NAME [\(j)]: " + testTargetName + t.testName + " *** START")
+            XCTAssertThrowsError(try whenIsNextTime(t.fromTime, amongMinutePresets: t.amongMinutePresets, delayedBy: 0))
+            print("TEST_NAME [\(j)]: " + testTargetName + t.testName + " *** END")
+            j += 1
+        }
+    }
+    
     func testNextMinuteDigit() {
         struct Test {
             let testName: String
             let currentMin: Int
             let minPresets: [Int]
-            let expectedOutput: Int
+            let expectedOutput: Int?
+            let expectedError: ErrorType?
         }
         let minPresets = [0, 20, 40, 45, 55]
         let toTestExpectingNoError = [
-            Test(testName: "nextMinuteDigit *** NoError, greater digit found", currentMin: 43, minPresets: minPresets, expectedOutput: 45),
-            Test(testName: "nextMinuteDigit *** NoError, less digit found", currentMin: 56, minPresets: minPresets, expectedOutput: 0),
-            Test(testName: "nextMinuteDigit *** NoError, digit from preset", currentMin: 20, minPresets: minPresets, expectedOutput: 40)
+            Test(testName: "NoError, greater digit found", currentMin: 43, minPresets: minPresets, expectedOutput: 45, expectedError: nil),
+            Test(testName: "NoError, less digit found", currentMin: 56, minPresets: minPresets, expectedOutput: 0, expectedError: nil),
+            Test(testName: "NoError, digit from preset", currentMin: 20, minPresets: minPresets, expectedOutput: 40, expectedError: nil)
         ]
         var i = 0
+        let testTargetName = "nextMinuteDigit *** "
         for t in toTestExpectingNoError {
-            print("TEST_NAME [\(i)]: " + t.testName + " *** START")
+            print("TEST_NAME [\(i)]: " + testTargetName + t.testName + " *** START")
             XCTAssertEqual(try! nextMinuteDigit(t.currentMin, minPresets: t.minPresets), t.expectedOutput)
-            print("TEST_NAME [\(i)]: " + t.testName + " *** END")
+            print("TEST_NAME [\(i)]: " + testTargetName + t.testName + " *** END")
             i += 1
+        }
+        let toTestExpectingError = [
+            Test(testName: "Error, empty minPresets input", currentMin: 43, minPresets: [], expectedOutput: nil, expectedError: TimeError.InvalidInput("Invalid input: preset minute digit array should not be empty")),
+            Test(testName: "Error, out of range input", currentMin: 60, minPresets: minPresets, expectedOutput: nil, expectedError: TimeError.InvalidInput("Invalid input: minute digit: 60")),
+            Test(testName: "Error, out of range input", currentMin: 0, minPresets: [99, 7], expectedOutput: nil, expectedError: TimeError.InvalidInput("Invalid input: minute digit: 99"))
+        ]
+        var j = 0
+        for t in toTestExpectingError {
+            print("TEST_NAME [\(j)]: " + testTargetName + t.testName + " *** START")
+            XCTAssertThrowsError(try nextMinuteDigit(t.currentMin, minPresets: t.minPresets))
+            print("TEST_NAME [\(j)]: " + testTargetName + t.testName + " *** END")
+            j += 1
         }
     }
     
