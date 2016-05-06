6cf6f330cbc54f11e6851ca7aac5355415f1d3b4
liweiz <matt.z.lw@gmail.com>
2016-05-06 01:31:04 -0400

    WIP: restarted to rewrite data model

--- TextCtxTransitionAnimation/TextCtxTransitionAnimation/TransitionModel.swift
+++ TextCtxTransitionAnimation/TextCtxTransitionAnimation/TransitionModel.swift
@@ -9,6 +9,39 @@
 import Foundation
 import UIKit
 
+protocol OneMovableOnX {
+    var maxDeltaX: CGFloat { get }
+}
+
+protocol ManyMovablesOnX {
+    var firstMovableElementInOrder: Any? { get }
+    var movablesInOrder: [Any] { get }
+}
+
+protocol MaxMoveInMany {
+    func maxMoveThatAllCan(maxMovesOfEach m: [CGFloat]) -> CGFloat
+}
+
+typealias XsNow = [CGFloat]
+
+typealias DeltaXsForAllLinesInSingleMove = [CGFloat]
+
+typealias DeltaXs = [DeltaXsForAllLinesInSingleMove]
+
+/// A container for fixed origin.xs for a line.
+struct StaticOriginXsOfLine {
+    let textImitatedInThisLine: CGFloat
+    let ctxImitatedInThisLine: CGFloat
+    let whenExtraHidden: CGFloat
+    
+    
+}
+
+
+
+
+
+
 
 struct NumbersInTextCtxHorizontalTransition {
     let originXsImitatingText: [CGFloat]
@@ -41,7 +74,10 @@ struct NumbersInTextCtxHorizontalTransition {
     }
     func maxDeltaAvailableNow(isForExtra: Bool = false, isReversed: Bool = false) -> (startIndex: Int, endIndex: Int, delta: CGFloat) {
         makeSureSelfIsValid()
+        let n = originXsNow.count
         for i in 0..<originXsNow.count {
+            let index = isReversed ? n - (i + 1) : i
+            let deltaNow = maxDeltaNow(index, isForExtra: isForExtra, isReversed: isReversed)
             
         }
     }
