4d9b07caed77269ba8a59acacbbb8e285a5f37cc
liweiz <matt.z.lw@gmail.com>
2016-05-26 14:33:34 -0400

    Completed SequenceType extension to generator new nested sequence.

--- TextCtxTransitionAnimation/TextCtxTransitionAnimation/Model.swift
+++ TextCtxTransitionAnimation/TextCtxTransitionAnimation/Model.swift
@@ -43,10 +43,41 @@ extension UInt16: Numberable {}
 extension UInt32: Numberable {}
 extension UInt64: Numberable {}
 
-protocol ElementOfSequenceTypesAtSamePositionGroupable {
-    associatedtype Sequence: CollectionType
-    associatedtype SequenceElement
-    @warn_unused_result func groupable(sequences: [Sequence]) -> Bool
-    @warn_unused_result func groupSequences(sequences: [Sequence], at position: Sequence.Index) -> [SequenceElement]?
-    @warn_unused_result func groupSequences(sequences: [Sequence]) -> [[SequenceElement]]?
+/// Group every same position elements of the nested sequences to new sequence.
+/// Shortest nested sequence sets the length of the new sequence.
+/// Example: [[1, 2, 3], [4, 5, 6], [7, 8]] => [[1, 4, 7], [2, 5, 8]]
+extension SequenceType where Generator.Element : SequenceType {
+    /// Returns Generators of nested sequences.
+    @warn_unused_result
+    func generatorsOfElements() -> [Generator.Element.Generator] {
+        return map { $0.generate() }
+    }
+    
+    /// Returns all elements from next() by given Generators.
+    /// Returns nil, if any next() is nil.
+    @warn_unused_result
+    func nextElements(from generators: [Generator.Element.Generator]) -> ([Generator.Element.Generator.Element], [Generator.Element.Generator])? {
+        var elements: [Generator.Element.Generator.Element] = []
+        var newGenerators: [Generator.Element.Generator] = []
+        for gen in generators {
+            var newGen = gen
+            guard let nextElement = newGen.next() else { return nil }
+            elements.append(nextElement)
+            newGenerators.append(newGen)
+        }
+        return (elements, newGenerators)
+    }
+    
+    /// Returns new nested array with each element is an array of same position
+    /// elements.
+    /// Shortest nested sequence sets the length of the new array.
+    var eachElementToSamePositionElements: [[Generator.Element.Generator.Element]] {
+        var generators = generatorsOfElements()
+        var newSequence: [[Generator.Element.Generator.Element]] = []
+        while let result = nextElements(from: generators) {
+            generators = result.1
+            newSequence.append(result.0)
+        }
+        return newSequence
+    }
 }
